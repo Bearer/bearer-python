74a6c46da7f3e4104688a63c51d432c3bd1c4f8b
David Roe <didroe@gmail.com>
2019-06-07 14:33:09 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bearer",
-    version="0.0.4",
+    version="1.0.0",
     author="Bearer Team",
     author_email="engineering+python@bearer.sh",
     description="Bearer python helper",
