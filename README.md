a0c3602d967c760d6c6c4297eface4755d7c585d
David Roe <dave.roe@bearer.sh>
2019-07-11 11:37:44 +0100

    feat: add support for proxy requests

--- README.md
+++ README.md
@@ -2,29 +2,55 @@
 
 Bearer Python client
 
-Installation:
+## Installation
 
 ```
 pip install bearer
 ```
 
-Example usage:
+## Usage
 
-```
+Get your Bearer [API Key](https://app.bearer.sh/keys) and integration id from
+the [Dashboard](https://app.bearer.sh) and use the Bearer client as follows:
+
+### Calling any APIs
+
+```python
 from bearer import Bearer
 
-bearer = Bearer('<apiKey>')
+bearer = Bearer('your api key') # find it on https://app.bearer.sh/keys
+github = (
+    bearer
+      .integration('your integration id') # you'll find it on the Bearer dashboard https://app.bearer.sh
+      .auth('your auth id') # Create an auth id for your integration via the dashboard
+)
 
-# Invoke a function that doesn't need auth details
-bearer.invoke('<buid>', '<functionName>')
-
-# Invoke a function using OAuth
-bearer.invoke('<buid>', '<functionName>', params={ 'authId': '<authId>' })
-
-# For a function using Basic/API Key auth
-bearer.invoke('<buid>', '<functionName>', params={ 'setupId': '<setupId>' })
-
-# Invoke a function with a body payload
-bearer.invoke('<buid>', '<functionName>', body={ 'some': 'data' })
-
+print(github.get('/repositories').json())
 ```
+
+We use [requests](https://2.python-requests.org/en/master/) internally and we
+return the response from this library from the request methods (`request`,
+`get`, `head`, `post`, `put`, `patch`, `delete`).
+
+More advanced examples:
+
+```python
+# With query parameters
+print(github.get('/repositories', query={ 'since': 364 }).json())
+
+# With body data
+print(github.post('/user/repos', body={ 'name': 'Just setting up my Bearer.sh' }).json())
+```
+
+### Calling custom functions
+
+```python
+from bearer import Bearer
+
+bearer = Bearer('your api key')
+github = bearer.integration('your integration id')
+
+print(github.invoke('your function name'))
+```
+
+[Learn more](https://docs.bearer.sh/working-with-bearer/manipulating-apis) on how to use custom functions with Bearer.sh.
--- tests/test_client.py
+++ tests/test_client.py
@@ -37,3 +37,94 @@ def test_invoke_raises_on_error_response(requests_mock):
 
     with pytest.raises(FunctionError, match='Oops!'):
         client.invoke(BUID, FUNCTION_NAME)
+
+
+PROXY_URL = 'https://int.bearer.sh/api/v4/functions/backend/{}/bearer-proxy'.format(BUID)
+ENDPOINT_URL = '{}/test?query=param'.format(PROXY_URL)
+
+HEADERS = { 'test': 'header' }
+SENT_HEADERS = { 'Bearer-Proxy-test': 'header' }
+QUERY = { 'query': 'param' }
+BODY = { 'body': 'data' }
+
+def test_request_supports_get(requests_mock):
+    requests_mock.get(ENDPOINT_URL, headers=SENT_HEADERS, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.get('/test', headers=HEADERS, query=QUERY)
+
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_supports_head(requests_mock):
+    requests_mock.head(ENDPOINT_URL, headers=SENT_HEADERS)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.head('/test', headers=HEADERS, query=QUERY)
+
+    assert response.status_code == 200
+
+def test_request_supports_post(requests_mock):
+    requests_mock.post(ENDPOINT_URL, headers=SENT_HEADERS, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.post('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert requests_mock.last_request.json() == BODY
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_supports_put(requests_mock):
+    requests_mock.put(ENDPOINT_URL, headers=SENT_HEADERS, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.put('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert requests_mock.last_request.json() == BODY
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_supports_patch(requests_mock):
+    requests_mock.patch(ENDPOINT_URL, headers=SENT_HEADERS, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.patch('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert requests_mock.last_request.json() == BODY
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_supports_delete(requests_mock):
+    requests_mock.delete(ENDPOINT_URL, headers=SENT_HEADERS, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID)
+    response = integration.delete('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert requests_mock.last_request.json() == BODY
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_passes_auth_id(requests_mock):
+    auth_id = 'test-auth-id'
+    expected_headers = { **SENT_HEADERS, 'Bearer-Auth-Id': auth_id }
+    requests_mock.post(ENDPOINT_URL, headers=expected_headers, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID).auth(auth_id)
+
+    response = integration.post('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert response.json() == SUCCESS_PAYLOAD
+
+def test_request_passes_setup_id(requests_mock):
+    setup_id = 'test-setup-id'
+    expected_headers = { **SENT_HEADERS, 'Bearer-Setup-Id': setup_id }
+    requests_mock.post(ENDPOINT_URL, headers=expected_headers, json=SUCCESS_PAYLOAD)
+
+    client = Bearer(API_KEY)
+    integration = client.integration(BUID).setup(setup_id)
+
+    response = integration.post('/test', headers=HEADERS, query=QUERY, body=BODY)
+
+    assert response.json() == SUCCESS_PAYLOAD
